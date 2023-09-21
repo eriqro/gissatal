77bc1b7a7a990cd0162c1cc6b65d0502475a53a5
eriqro <142878819+eriqro@users.noreply.github.com>
2023-09-21 13:23:34 +0200

    färdig

--- gissataltest.py
+++ gissataltest.py
@@ -1,32 +1,25 @@
 import random
-print("Du ska gissa talet, det kan vara vad som hälst från 1 - 100   Du får 7 försök på dig")
-tal=random.randint(1,100)
-print(tal)
-
-totalg=1
-igen=True
-while igen:
-        try:
-            gissning=int(input("Gissa talet: "))
-        except:
-            print("Fel, Du måste ha ett heltal, inga bokstäver eller decimaltal ")
-
-        if totalg==7:
-            igen=False
-        elif gissning==tal:
-            print("Du gissade rätt efter ",totalg," gissningar")
-            totalg=totalg+1
-            igen2=input("Svara ja om du vill fortsätta och nej om du inte vill: ").lower()
-            if igen2=="ja":
-                igen=True
-            elif igen2=="nej":
-                igen=False
-            totalg=1
-            tal=random.randint(1,100)
-            print(tal)
-        elif gissning<tal:
-            print("Du måste gå högre!")
-            totalg=totalg+1
-        elif gissning>tal:
-            print("Du måste gå lägre!")
-            totalg=totalg+1
+print(f"Du ska gissa talet, det kan vara vad som hälst från 1 - 100 \nDu får 7 försök på dig")
+number=random.randint(1,100)
+print(number)
+a=True
+guesses=7
+while guesses>0:
+    try:
+        guess=int(input("Din gissning!: "))
+        if guess==number:
+            guesses-=1
+            print(f"Du gissa rätt på ditt {7-guesses} försök\n")
+            break
+        elif guesses==1:
+            print("Du har tyvärr fått slut på försök")
+            break
+        elif guess>number:
+            guesses-=1
+            print(f"Du gissade för högt \nDu har {guesses} gissningar kvar\n")
+        elif guess<number:
+            guesses-=1
+            print(f"Du gissade för lågt \nDu har {guesses} gissningar kvar\n")
+    except ValueError:
+        print(f"Snälla använd nummer och inget annat :p \n")
+    
