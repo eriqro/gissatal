a95d905b278e772aa922c21679f35fb9e2fa01c4
eriqro <142878819+eriqro@users.noreply.github.com>
2023-09-28 13:38:24 +0200

    helt färdig

--- gissataltest.py
+++ gissataltest.py
@@ -1,10 +1,13 @@
 import random
+import os
+os.system('cls')
 a=True
 b=True
+
 while b==True:
-    print(f"\nDu ska gissa talet, det kan vara vad som hälst från 1 - 100 \nDu får 7 försök på dig\n")
+    os.system('cls')
+    print(f"\nDu ska gissa talet, det kan vara vad som hälst från 1 - 100 \n\nDu kan alltid avluta spelet mitt i om du skriver något nummer som inte är mellan 1-100\n\nDu får 7 försök på dig\n")
     number=random.randint(1,100)
-    print(number)
     guesses=7
     while guesses>0:
         try:
@@ -19,8 +22,18 @@ while b==True:
                     quit()
                 else:
                     break
+            elif guess<0 or guess>100:
+                print("\nHejdå!")
+                quit()
             elif guesses==1:
                 print("Du har tyvärr fått slut på försök")
+                again=input("Vill du spela igen? j/n: ").lower()
+                if again=="n" or again=="nej":
+                    b=False
+                    print("\nHejdå")
+                    quit()
+                else:
+                    break
             elif guess>number:
                 guesses-=1
                 print(f"Du gissade för högt \nDu har {guesses} gissningar kvar\n")
